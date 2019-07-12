24e5b48dbeb082f3d15ae344ca06260f5958d16d
Fahim Rashid <fahimrashid790@gmail.com>
2019-07-12 14:25:45 -0700

    added functionality to navbar

--- src/app/here-map/here-map.component.ts
+++ src/app/here-map/here-map.component.ts
@@ -1,169 +1,170 @@
 import { Component, OnInit, ViewChild, ElementRef, Input } from '@angular/core';
 import { EVENT_MANAGER_PLUGINS } from '@angular/platform-browser';
 
-var fist = new H.map.Icon("../assets/fist.png", {size: {w: 35, h: 35}});
-var rob = new H.map.Icon("../assets/bandit.png", {size: {w: 35, h: 35}});
-var theft = new H.map.Icon("../assets/theft.png", {size: {w: 35, h: 35}});
+var fist = new H.map.Icon("../assets/fist.png", { size: { w: 35, h: 35 } });
+var rob = new H.map.Icon("../assets/bandit.png", { size: { w: 35, h: 35 } });
+var theft = new H.map.Icon("../assets/theft.png", { size: { w: 35, h: 35 } });
 declare var H: any;
 
 @Component({
-    selector: 'here-map',
-    templateUrl: './here-map.component.html',
-    styleUrls: ['./here-map.component.scss']
+  selector: 'here-map',
+  templateUrl: './here-map.component.html',
+  styleUrls: ['./here-map.component.scss']
 })
 export class HereMapComponent implements OnInit {
 
-    @ViewChild("map")
-    public mapElement: ElementRef;
+  @ViewChild("map")
+  public mapElement: ElementRef;
 
-    @Input()
-    public appId: any;
+  @Input()
+  public appId: any;
 
-    @Input()
-    public appCode: any;
+  @Input()
+  public appCode: any;
 
-    @Input()
-    public lat: any;
+  @Input()
+  public lat: any;
 
-    @Input()
-    public lng: any;
+  @Input()
+  public lng: any;
 
-    private platform: any;
-    private map: any;
-    private router: any;
-
-
-
-    public constructor() { }
-
-    public ngOnInit() {
-        this.platform = new H.service.Platform({
-            "app_id": this.appId,
-            "app_code": this.appCode
-        });
-        this.router = this.platform.getRoutingService();
-    }
-    
-
-    public ngAfterViewInit() {
-        setTimeout(() => {
-            let defaultLayers = this.platform.createDefaultLayers();
-            this.map = new H.Map(
-                this.mapElement.nativeElement,
-                defaultLayers.normal.map,
-                {
-                    zoom: 14,
-                    tilt: 45,
-                    center: { lat: this.lat, lng: this.lng }
-                }
-            );
-
-            const ui = H.ui.UI.createDefault(this.map, defaultLayers);
-            const marker1 = new H.map.Marker({ lat: 34.023370, lng: -118.274240}, { icon: theft });
-            marker1.setData("<p>Theft <br> 900 BLOCK W 30TH ST</p>");
-            marker1.addEventListener("tap", event => {
-              const bubble = new H.ui.InfoBubble(
-                event.target.getPosition(),
-                {
-                  content: event.target.getData()
-                }
-              ); 
-              ui.addBubble(bubble);
-            });
+  private platform: any;
+  private map: any;
+  private router: any;
 
 
-            const marker2 = new H.map.Marker({ lat: 34.021960, lng: -118.279970}, { icon: theft });
-            marker2.setData("<p>Theft <br> 3200 BLOCK S HOOVER ST</p>");
-            marker2.addEventListener("tap", event => {
-              const bubble = new H.ui.InfoBubble(
-                event.target.getPosition(),
-                {
-                  content: event.target.getData()
-                }
-              ); 
-              ui.addBubble(bubble);
-            });
 
-            const marker3 = new H.map.Marker({ lat: 34.026270, lng: -118.298850}, { icon: rob });
-            marker3.setData("<p> Robbery <br> VERMONT AV & 30TH ST</p>");
-            marker3.addEventListener("tap", event => {
-              const bubble = new H.ui.InfoBubble(
-                event.target.getPosition(),
-                {
-                  content: event.target.getData()
-                }
-              ); 
-              ui.addBubble(bubble);
-            });
+  public constructor() { }
 
-            const marker4 = new H.map.Marker({ lat: 34.022130, lng: -118.292300}, { icon: theft });
-            marker4.setData("<p> Theft <br> 1100 BLOCK W 36TH PL </p>");
-            marker4.addEventListener("tap", event => {
-              const bubble = new H.ui.InfoBubble(
-                event.target.getPosition(),
-                {
-                  content: event.target.getData()
-                }
-              ); 
-              ui.addBubble(bubble);
-            });
+  public ngOnInit() {
+    this.platform = new H.service.Platform({
+      "app_id": 'JIhWsj5ocJ55IpNQqmiM',
+      "app_code": 'Dl3qLVq0RJn-RBmD5LRviw'
+    });
+    this.router = this.platform.getRoutingService();
+  }
 
-            const marker5 = new H.map.Marker({ lat: 34.023369, lng: -118.274239}, { icon: rob });
-            marker5.setData("<p>Robbery <br> 1500 BLOCK W 30TH ST </p>");
-            marker5.addEventListener("tap", event => {
-              const bubble = new H.ui.InfoBubble(
-                event.target.getPosition(),
-                {
-                  content: event.target.getData()
-                }
-              ); 
-              ui.addBubble(bubble);
-            });
 
-            const marker6 = new H.map.Marker({ lat: 34.018850, lng: -118.281026}, { icon: fist });
-            marker6.setData("<p> Assault <br> FIGUEROA & JEFFERSON </p>");
-            marker6.addEventListener("tap", event => {
-              const bubble = new H.ui.InfoBubble(
-                event.target.getPosition(),
-                {
-                  content: event.target.getData()
-                }
-              ); 
-              ui.addBubble(bubble);
-            });
-            
-                this.map.addObjects([marker1, marker2, marker3, marker4, marker5, marker6 ]);
-            let behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(this.map));
-        }, 100);
-    }
+  public ngAfterViewInit() {
+    setTimeout(() => {
+      let defaultLayers = this.platform.createDefaultLayers();
+      this.map = new H.Map(
+        this.mapElement.nativeElement,
+        defaultLayers.normal.map,
+        {
+          zoom: 14,
+          tilt: 45,
+          center: { lat: this.lat, lng: this.lng }
+        }
+      );
 
-    public isoline() { 
-      let params = {
-        "mode": "fastest;car;traffic:enabled",
-        "range": "300",
-        "rangetype": "time",
-        "departure": "now",
-        "start": this.lat + "," + this.lng
+      const ui = H.ui.UI.createDefault(this.map, defaultLayers);
+      const marker1 = new H.map.Marker({ lat: 34.023370, lng: -118.274240 }, { icon: theft });
+      marker1.setData("<p>Theft <br> 900 BLOCK W 30TH ST</p>");
+      marker1.addEventListener("tap", event => {
+        const bubble = new H.ui.InfoBubble(
+          event.target.getPosition(),
+          {
+            content: event.target.getData()
+          }
+        );
+        ui.addBubble(bubble);
+      });
+
+
+      const marker2 = new H.map.Marker({ lat: 34.021960, lng: -118.279970 }, { icon: theft });
+      marker2.setData("<p>Theft <br> 3200 BLOCK S HOOVER ST</p>");
+      marker2.addEventListener("tap", event => {
+        const bubble = new H.ui.InfoBubble(
+          event.target.getPosition(),
+          {
+            content: event.target.getData()
+          }
+        );
+        ui.addBubble(bubble);
+      });
+
+      const marker3 = new H.map.Marker({ lat: 34.026270, lng: -118.298850 }, { icon: rob });
+      marker3.setData("<p> Robbery <br> VERMONT AV & 30TH ST</p>");
+      marker3.addEventListener("tap", event => {
+        const bubble = new H.ui.InfoBubble(
+          event.target.getPosition(),
+          {
+            content: event.target.getData()
+          }
+        );
+        ui.addBubble(bubble);
+      });
+
+      const marker4 = new H.map.Marker({ lat: 34.022130, lng: -118.292300 }, { icon: theft });
+      marker4.setData("<p> Theft <br> 1100 BLOCK W 36TH PL </p>");
+      marker4.addEventListener("tap", event => {
+        const bubble = new H.ui.InfoBubble(
+          event.target.getPosition(),
+          {
+            content: event.target.getData()
+          }
+        );
+        ui.addBubble(bubble);
+      });
+
+      const marker5 = new H.map.Marker({ lat: 34.023369, lng: -118.274239 }, { icon: rob });
+      marker5.setData("<p>Robbery <br> 1500 BLOCK W 30TH ST </p>");
+      marker5.addEventListener("tap", event => {
+        const bubble = new H.ui.InfoBubble(
+          event.target.getPosition(),
+          {
+            content: event.target.getData()
+          }
+        );
+        ui.addBubble(bubble);
+      });
+
+      const marker6 = new H.map.Marker({ lat: 34.018850, lng: -118.281026 }, { icon: fist });
+      marker6.setData("<p> Assault <br> FIGUEROA & JEFFERSON </p>");
+      marker6.addEventListener("tap", event => {
+        const bubble = new H.ui.InfoBubble(
+          event.target.getPosition(),
+          {
+            content: event.target.getData()
+          }
+        );
+        ui.addBubble(bubble);
+      });
+
+      this.map.addObjects([marker1, marker2, marker3, marker4, marker5, marker6]);
+      let behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(this.map));
+    }, 100);
+  }
+
+
+  public isoline() {
+    let params = {
+      "mode": "fastest;car;traffic:enabled",
+      "range": "300",
+      "rangetype": "time",
+      "departure": "now",
+      "start": this.lat + "," + this.lng
     }
     this.map.removeObjects(this.map.getObjects());
     this.router.calculateIsoline(params, data => {
-        if(data.response) {
-            let center = new H.geo.Point(data.response.center.latitude, data.response.center.longitude),
-                isolineCoords = data.response.isoline[0].component[0].shape,
-                linestring = new H.geo.LineString(),
-                isolinePolygon,
-                isolineCenter;
-            isolineCoords.forEach(coords => {
-                linestring.pushLatLngAlt.apply(linestring, coords.split(','));
-            });
-            isolinePolygon = new H.map.Polygon(linestring);
-            isolineCenter = new H.map.Marker(center);
-            this.map.addObjects([isolineCenter, isolinePolygon]);
-            this.map.setViewBounds(isolinePolygon.getBounds());
-        }
+      if (data.response) {
+        let center = new H.geo.Point(data.response.center.latitude, data.response.center.longitude),
+          isolineCoords = data.response.isoline[0].component[0].shape,
+          linestring = new H.geo.LineString(),
+          isolinePolygon,
+          isolineCenter;
+        isolineCoords.forEach(coords => {
+          linestring.pushLatLngAlt.apply(linestring, coords.split(','));
+        });
+        isolinePolygon = new H.map.Polygon(linestring);
+        isolineCenter = new H.map.Marker(center);
+        this.map.addObjects([isolineCenter, isolinePolygon]);
+        this.map.setViewBounds(isolinePolygon.getBounds());
+      }
     }, error => {
-        console.error(error);
+      console.error(error);
     });
-    }
+  }
 
 }
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +8,41 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  results = [];
+  public myInput;
+  constructor(public http: HttpClient) { }
 
-  constructor() {}
+  onInput(val) {
+    console.log(val);
+    console.log(this.myInput);
+  }
 
-}
+  onChange(e) {
+    const value = e.target.value
+    console.log(value);
+
+    const url = `http://autocomplete.geocoder.api.here.com/6.2/suggest.json?query=${value}&app_id=JIhWsj5ocJ55IpNQqmiM&app_code=Dl3qLVq0RJn-RBmD5LRviw`;
+    this.http.get(url)
+    .subscribe((result: any) => {
+
+      console.log(result);
+      if(result.suggestions){
+        const data = result.suggestions.filter((item) => {
+          if(item.address.houseNumber){
+            return item;
+          } 
+        })
+        this.results = data;
+
+      }else{
+        this.results = [];
+      }
+
+    }, (err) => {
+      console.log(err);
+      this.results;
+    })
+
+  }
+
+};

1da5c03e95b6b9b09bb3b6c9e79b1a159786b396
Fahim Rashid <fahimrashid790@gmail.com>
2019-07-12 09:44:26 -0700

    update

--- src/app/here-map/here-map.component.ts
+++ src/app/here-map/here-map.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, ViewChild, ElementRef, Input } from '@angular/core';
+import { EVENT_MANAGER_PLUGINS } from '@angular/platform-browser';
 
+var fist = new H.map.Icon("../assets/fist.png", {size: {w: 35, h: 35}});
+var rob = new H.map.Icon("../assets/bandit.png", {size: {w: 35, h: 35}});
+var theft = new H.map.Icon("../assets/theft.png", {size: {w: 35, h: 35}});
 declare var H: any;
 
 @Component({
@@ -39,6 +43,7 @@ export class HereMapComponent implements OnInit {
         });
         this.router = this.platform.getRoutingService();
     }
+    
 
     public ngAfterViewInit() {
         setTimeout(() => {
@@ -53,12 +58,81 @@ export class HereMapComponent implements OnInit {
                 }
             );
 
-            const marker1 = new H.map.Marker({ lat: 34.023370, lng: -118.274240});
-            const marker2 = new H.map.Marker({ lat: 34.021960, lng: -118.279970});
-            const marker3 = new H.map.Marker({ lat: 34.026270, lng: -118.298850});
-            const marker4 = new H.map.Marker({ lat: 34.022130, lng: -118.292300});
-            const marker5 = new H.map.Marker({ lat: 34.066710, lng: -118.284330});
-                this.map.addObjects([marker1, marker2, marker3, marker4, marker5]);
+            const ui = H.ui.UI.createDefault(this.map, defaultLayers);
+            const marker1 = new H.map.Marker({ lat: 34.023370, lng: -118.274240}, { icon: theft });
+            marker1.setData("<p>Theft <br> 900 BLOCK W 30TH ST</p>");
+            marker1.addEventListener("tap", event => {
+              const bubble = new H.ui.InfoBubble(
+                event.target.getPosition(),
+                {
+                  content: event.target.getData()
+                }
+              ); 
+              ui.addBubble(bubble);
+            });
+
+
+            const marker2 = new H.map.Marker({ lat: 34.021960, lng: -118.279970}, { icon: theft });
+            marker2.setData("<p>Theft <br> 3200 BLOCK S HOOVER ST</p>");
+            marker2.addEventListener("tap", event => {
+              const bubble = new H.ui.InfoBubble(
+                event.target.getPosition(),
+                {
+                  content: event.target.getData()
+                }
+              ); 
+              ui.addBubble(bubble);
+            });
+
+            const marker3 = new H.map.Marker({ lat: 34.026270, lng: -118.298850}, { icon: rob });
+            marker3.setData("<p> Robbery <br> VERMONT AV & 30TH ST</p>");
+            marker3.addEventListener("tap", event => {
+              const bubble = new H.ui.InfoBubble(
+                event.target.getPosition(),
+                {
+                  content: event.target.getData()
+                }
+              ); 
+              ui.addBubble(bubble);
+            });
+
+            const marker4 = new H.map.Marker({ lat: 34.022130, lng: -118.292300}, { icon: theft });
+            marker4.setData("<p> Theft <br> 1100 BLOCK W 36TH PL </p>");
+            marker4.addEventListener("tap", event => {
+              const bubble = new H.ui.InfoBubble(
+                event.target.getPosition(),
+                {
+                  content: event.target.getData()
+                }
+              ); 
+              ui.addBubble(bubble);
+            });
+
+            const marker5 = new H.map.Marker({ lat: 34.023369, lng: -118.274239}, { icon: rob });
+            marker5.setData("<p>Robbery <br> 1500 BLOCK W 30TH ST </p>");
+            marker5.addEventListener("tap", event => {
+              const bubble = new H.ui.InfoBubble(
+                event.target.getPosition(),
+                {
+                  content: event.target.getData()
+                }
+              ); 
+              ui.addBubble(bubble);
+            });
+
+            const marker6 = new H.map.Marker({ lat: 34.018850, lng: -118.281026}, { icon: fist });
+            marker6.setData("<p> Assault <br> FIGUEROA & JEFFERSON </p>");
+            marker6.addEventListener("tap", event => {
+              const bubble = new H.ui.InfoBubble(
+                event.target.getPosition(),
+                {
+                  content: event.target.getData()
+                }
+              ); 
+              ui.addBubble(bubble);
+            });
+            
+                this.map.addObjects([marker1, marker2, marker3, marker4, marker5, marker6 ]);
             let behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(this.map));
         }, 100);
     }
